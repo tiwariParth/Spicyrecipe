0e366ba7b0446d708ee997c78567f3880b5ab5fa
tiwariParth <243mjparth@gmail.com>
2024-04-28 18:58:14 +0530

    Add console.log statement in backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,4 +14,5 @@ app.use("/api/recipes", recipeRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log("hello");
 });
