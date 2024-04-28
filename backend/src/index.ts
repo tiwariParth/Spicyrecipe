63d328d8382d76a1ec4d78891300ae35f4402f25
tiwariParth <243mjparth@gmail.com>
2024-04-28 18:59:07 +0530

    Remove unnecessary console.log statement in backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,5 +14,4 @@ app.use("/api/recipes", recipeRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log("hello");
 });
